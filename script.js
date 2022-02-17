677d9f984d90713bb483917903263db6a7d09142
Jasmeet <jasighotra123@gmail.com>
2022-02-17 11:29:17 +0530

    https

--- script.js
+++ script.js
@@ -20,123 +20,118 @@ const errorDes = document.querySelector(".p");
 //Api Calls
 async function apiCallBtn(location) {
   return fetch(
-    `http://api.weatherstack.com/current?access_key=e4b00e1b14c307c6ce0e20daf670b56f&query=${location}& forecast_days = 1`
+    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=bf99cb0065e369f731cfedfe08b80061`
   );
 }
 async function apiCallGps(lat, long) {
   return fetch(
-    `http://api.weatherstack.com/current?access_key=e4b00e1b14c307c6ce0e20daf670b56f&query=${lat},${long}& forecast_days = 1`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=bf99cb0065e369f731cfedfe08b80061`
   );
 }
 //Common api
 const commonApi = function (el) {
-  if (el.success == false) {
-    console.log("failed");
-    warning.style.visibility = "visible";
-    section1.style.filter = "blur(1rem)";
+  section2.style.transform = "translateX(0)";
+  section2.style.visibility = "visible";
+  section1.style.transform = "translateX(100vw)";
+  section1.style.display = "none";
+  p1CityName.textContent = el.name;
+  const day = new Date(el.dt * 1000);
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  p1Day.textContent = days[day.getDay()] + ",";
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const rain = [
+    300, 301, 302, 310, 311, 312, 313, 314, 321, 500, 501, 502, 503, 504, 511,
+    520, 521, 522, 531, 200, 201, 202, 210, 211, 212, 221, 230, 231, 232,
+  ];
+  const ice = [600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622];
+  const fog = [701, 711, 721, 731, 741, 751, 761, 762, 771, 781];
+  const cloud = [801, 802, 803, 804];
+
+  if (rain.includes(el.weather.id)) {
+    console.log("yes");
+
+    p1.style.backgroundImage =
+      "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('rain.gif')";
+  } else if (cloud.includes(el.weather.id)) {
+    console.log("yes");
+
+    p1.style.backgroundImage =
+      "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('clod.jpg')";
+  } else if (ice.includes(el.weather.id)) {
+    console.log("yes");
+
+    p1.style.backgroundImage =
+      "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('snow.gif')";
+  } else if (fog.includes(el.weather.id)) {
+    console.log("yes");
+
+    p1.style.backgroundImage =
+      "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('mist.gif')";
   } else {
-    section2.style.transform = "translateX(0)";
-    section2.style.visibility = "visible";
-    section1.style.transform = "translateX(100vw)";
-    section1.style.display = "none";
-    p1CityName.textContent = el.location.name;
-    const day = new Date(el.location.localtime);
-    const days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    p1Day.textContent = days[day.getDay()] + ",";
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    const rain = [
-      389, 386, 359, 356, 353, 314, 311, 308, 305, 302, 299, 296, 293, 284, 281,
-      266, 263, 176,
-    ];
-    const ice = [
-      395, 392, 377, 374, 371, 368, 365, 362, 350, 338, 335, 332, 329, 326, 323,
-      320, 317, 248, 230, 227, 182, 179,
-    ];
-    const fog = [260, 248, 143, 122];
-    const cloud = [119, 116];
-
-    if (rain.includes(el.current.weather_code)) {
-      console.log("yes");
-
-      p1.style.backgroundImage =
-        "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('rain.gif')";
-    } else if (cloud.includes(el.current.weather_code)) {
-      console.log("yes");
-
-      p1.style.backgroundImage =
-        "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('clod.jpg')";
-    } else if (ice.includes(el.current.weather_code)) {
-      console.log("yes");
-
-      p1.style.backgroundImage =
-        "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('snow.gif')";
-    } else if (fog.includes(el.current.weather_code)) {
-      console.log("yes");
-
-      p1.style.backgroundImage =
-        "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('mist.gif')";
-    } else {
-      console.log("yes");
-      p1.style.backgroundImage =
-        "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('sunny.gif')";
-    }
-    p1.style.backgroundSize = "cover";
-    if (day.getHours() > 6 && day.getHours() <= 19) {
-      section2.style.backgroundImage =
-        "linear-gradient(to right top, rgba(126, 212, 255, 0.5), rgba(73, 99, 217, 0.5) ),url('panel1bg.png')";
-    } else {
-      section2.style.backgroundImage =
-        "linear-gradient(to right top, rgba(145, 147, 148, 0.5), rgba(25, 25, 26, 0.5) ),url('panel1bg.png')";
-    }
-    p1Month.textContent = months[day.getMonth()] + " ";
-    p1Date.textContent = day.getDate() + ", " + day.getFullYear();
-    p1Temp.innerHTML =
-      el.current.temperature + "&#176" + "<span class='degree'>c</span>";
-    p1Weather.textContent = el.current.weather_descriptions;
-    document.querySelector("#prec").textContent = el.current.precip + "mm";
-    //loader
-    setTimeout(function () {
-      loader2.style.display = "none";
-    }, 2000);
+    console.log("yes");
+    p1.style.backgroundImage =
+      "linear-gradient(to right top, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url('sunny.gif')";
   }
+  p1.style.backgroundSize = "cover";
+  if (day.getHours() > 6 && day.getHours() <= 19) {
+    section2.style.backgroundImage =
+      "linear-gradient(to right top, rgba(126, 212, 255, 0.5), rgba(73, 99, 217, 0.5) ),url('panel1bg.png')";
+  } else {
+    section2.style.backgroundImage =
+      "linear-gradient(to right top, rgba(145, 147, 148, 0.5), rgba(25, 25, 26, 0.5) ),url('panel1bg.png')";
+  }
+  p1Month.textContent = months[day.getMonth()] + " ";
+  p1Date.textContent = day.getDate() + ", " + day.getFullYear();
+  //temp
+  p1Temp.innerHTML =
+    el.main.temp.toPrecision(3) + "&#176" + "<span class='degree'>c</span>";
+  p1Weather.textContent = el.weather.description;
+  document.querySelector("#prec").textContent = " " + el.main.humidity + "%";
+  // //loader
+  setTimeout(function () {
+    loader2.style.display = "none";
+  }, 2000);
 };
 //eventhandler for button
 const buttonMechanisum = function (el) {
   el.preventDefault();
   apiCallBtn(landingForm.value)
     .then((el) => {
-      console.log(el);
-
+      if (el.statusText == "Not Found") {
+        throw new Error("No such place exists");
+      }
       return el.json();
     })
     .then((el) => {
+      console.log(el);
       commonApi(el);
     })
     .catch((el) => {
-      errorDes.textContent = "Check your network connection";
+      errorDes.textContent = el;
       warning.style.visibility = "visible";
       section1.style.filter = "blur(1rem)";
+      console.log(el);
     });
 };
 //eventhandler for GPS
@@ -155,6 +150,9 @@ const locationMechanisum = function (el) {
             commonApi(el);
           })
           .catch((el) => {
+            errorDes.textContent = el;
+            warning.style.visibility = "visible";
+            section1.style.filter = "blur(1rem)";
             console.log(el);
           });
       },
