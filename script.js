cd9d2dd1d2be2fb807f057d85f60b1943642e6d8
Jasmeet Singh <jasighotra123@gmail.com>
2022-03-11 01:35:55 +0530

    mobile

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // VARIABLES....................................................
+//temp
+// https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.tubikstudio.com%2Fweather-in-ui-design-come-rain-or-shine%2F&psig=AOvVaw1y43NZi0flBnx2557IWAGS&ust=1646757413182000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjLyd23tPYCFQAAAAAdAAAAABAD
+const rain = [
+  300, 301, 302, 310, 311, 312, 313, 314, 321, 500, 501, 502, 503, 504, 511,
+  520, 521, 522, 531, 200, 201, 202, 210, 211, 212, 221, 230, 231, 232,
+];
+const ice = [600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622];
+const fog = [701, 711, 721, 731, 741, 751, 761, 762, 771, 781];
+const cloud = [801, 802, 803, 804];
+const panel3 = document.querySelector(".panel3");
+const panel3Heading = document.querySelector(".panel3-heading-edit");
+const panel3D1 = document.querySelector(".panel3-day1");
+const panel3D2 = document.querySelector(".panel3-day2");
+const panel3D3 = document.querySelector(".panel3-day3");
 const btnLanding = document.querySelector(".btn-landing");
 const section2 = document.querySelector(".section2");
 const landingForm = document.querySelector("#name");
@@ -27,12 +41,50 @@ const section2Con = document.querySelector(".section2Con");
 const mediaQuery = window.matchMedia("(max-width:560px)");
 const loader2 = document.querySelector(".loader2");
 const loader3 = document.querySelector(".loader3");
+const loader4 = document.querySelector(".loader4");
 let theMarker = undefined;
 const narrationContainer = document.querySelector(".nar-con");
 const narDot = document.querySelector(".nar-dot");
+const cloudIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" class="bi bi-cloud" viewBox="0 0 16 16">
+<path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+</svg>`;
+const cloudRain = `<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" class="bi bi-cloud-drizzle" viewBox="0 0 16 16">
+<path d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317zm.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973zM8.5 2a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4.002 4.002 0 0 1 8.5 2z"/>
+</svg>`;
+const snowIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" class="bi bi-cloud-snow" viewBox="0 0 16 16">
+<path d="M13.405 4.277a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 10.25H13a3 3 0 0 0 .405-5.973zM8.5 1.25a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1-.001 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4.002 4.002 0 0 1 8.5 1.25zM2.625 11.5a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm2.75 2a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm5.5 0a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm-2.75-2a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25zm5.5 0a.25.25 0 0 1 .25.25v.57l.501-.287a.25.25 0 0 1 .248.434l-.495.283.495.283a.25.25 0 0 1-.248.434l-.501-.286v.569a.25.25 0 1 1-.5 0v-.57l-.501.287a.25.25 0 0 1-.248-.434l.495-.283-.495-.283a.25.25 0 0 1 .248-.434l.501.286v-.569a.25.25 0 0 1 .25-.25z"/>
+</svg>`;
+const fogIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" class="bi bi-cloud-fog" viewBox="0 0 16 16">
+<path d="M3 13.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm10.405-9.473a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 12H13a3 3 0 0 0 .405-5.973zM8.5 3a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4.002 4.002 0 0 1 8.5 3z"/>
+</svg>`;
+const sunIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" class="bi bi-brightness-high" viewBox="0 0 16 16">
+<path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+</svg>`;
 let narratinArray = [];
-
 let speak = "";
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 //map marker
 let theMarkericon = L.icon({
   iconUrl: "mapicon.png",
@@ -42,39 +94,10 @@ let theMarkericon = L.icon({
 let temp = 0;
 //panelUI
 const panelUI = function (el) {
-  p1CityName.textContent = el.name;
+  p1CityName.textContent = panel3Heading.textContent = el.name;
   const day = new Date(el.dt * 1000);
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+
   p1Day.textContent = days[day.getDay()] + ",";
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const rain = [
-    300, 301, 302, 310, 311, 312, 313, 314, 321, 500, 501, 502, 503, 504, 511,
-    520, 521, 522, 531, 200, 201, 202, 210, 211, 212, 221, 230, 231, 232,
-  ];
-  const ice = [600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622];
-  const fog = [701, 711, 721, 731, 741, 751, 761, 762, 771, 781];
-  const cloud = [801, 802, 803, 804];
 
   if (rain.includes(el.weather[0].id)) {
     p1.style.backgroundImage =
@@ -191,6 +214,101 @@ const pollutionUi = function (el) {
     document.querySelector(".triangle-alt").style.backgroundColor = "#7E0023";
   }
 };
+
+//forcast UI
+const forcastUI = function (el) {
+  console.log(el.list[1].weather[0].id);
+  for (let j = 1; j <= 3; j++) {
+    if (rain.includes(el.list[j].weather[0].id)) {
+      document.querySelector(`.panel3-icon-${j}`).innerHTML = cloudRain;
+      document.querySelector(`.panel3-icon-${j}`).style.color = "darkblue";
+    } else if (cloud.includes(el.list[j].weather[0].id)) {
+      document.querySelector(`.panel3-icon-${j}`).innerHTML = cloudIcon;
+      document.querySelector(`.panel3-icon-${j}`).style.color =
+        "rgb(59, 170, 221)";
+    } else if (ice.includes(el.list[j].weather[0].id)) {
+      document.querySelector(`.panel3-icon-${j}`).innerHTML = snowIcon;
+      document.querySelector(`.panel3-icon-${j}`).style.color = "white";
+    } else if (fog.includes(el.list[j].weather[0].id)) {
+      document.querySelector(`.panel3-icon-${j}`).innerHTML = fogIcon;
+      document.querySelector(`.panel3-icon-${j}`).style.color = "gray";
+    } else {
+      document.querySelector(`.panel3-icon-${j}`).innerHTML = sunIcon;
+      document.querySelector(`.panel3-icon-${j}`).style.color = "yellow";
+    }
+
+    document.querySelector(`.panel3-temp-${j}`).innerHTML =
+      el.list[j].main.temp.toPrecision(3) +
+      "&#176" +
+      "<span class='degree'>C</span>";
+    document.querySelector(`.panel3-weather-${j}`).textContent =
+      el.list[j].weather[0].main;
+  }
+  let ind = 1;
+  for (let j = 2; j <= 18; j += 8) {
+    let dayz = new Date(el.list[j].dt_txt);
+    document.querySelector(
+      `.panel3-date-${ind}`
+    ).innerHTML = `${dayz.getDate()} ${
+      months[dayz.getMonth()]
+    } <span id='day${ind}'> ${days[dayz.getDay()]}</span>`;
+
+    ind += 1;
+  }
+};
+//forcast btn
+async function forcastBtn(location) {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&APPID=bf99cb0065e369f731cfedfe08b80061`
+  );
+}
+//forcast gps
+async function forcastGps(lat, long) {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=metric&appid=bf99cb0065e369f731cfedfe08b80061`
+  );
+}
+//forcast btn call
+const forcastBtnCall = function (location) {
+  forcastBtn(location)
+    .then((el) => {
+      if (el.status === 200) {
+        return el.json();
+      } else {
+        throw "Something went wrong";
+      }
+    })
+    .then((el) => {
+      console.log(el);
+      forcastUI(el);
+    })
+    .catch((el) => {
+      errorDes.textContent = el;
+      warning.style.visibility = "visible";
+      section1.style.filter = "blur(1rem)";
+      console.log(el);
+    });
+};
+//forcast gps call
+const forcastGpsCall = function (lat, long) {
+  forcastGps(lat, long)
+    .then((el) => {
+      if (el.status === 200) {
+        return el.json();
+      } else {
+        throw "Something went wrong";
+      }
+    })
+    .then((el) => {
+      forcastUI(el);
+    })
+    .catch((el) => {
+      errorDes.textContent = el;
+      warning.style.visibility = "visible";
+      section1.style.filter = "blur(1rem)";
+      console.log(el);
+    });
+};
 //pollution btn
 async function commonPollutionBtn(location) {
   return fetch(
@@ -237,12 +355,14 @@ const pollutionFunGps = function (lat, long) {
 //Api Calls
 async function apiCallBtn(location) {
   pollutionFunBtn(location);
-  return fetch(
+  forcastBtnCall(location);
+  return await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=bf99cb0065e369f731cfedfe08b80061`
   );
 }
 async function apiCallGps(lat, long) {
   pollutionFunGps(lat, long);
+  forcastGpsCall(lat, long);
   return fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=bf99cb0065e369f731cfedfe08b80061`
   );
@@ -454,6 +574,7 @@ const slider = function () {
     goToSlide(curSlide);
     activateDot(curSlide);
   };
+  setInterval(nextSlide, 8000);
 
   const prevSlide = function () {
     if (curSlide === 0) {
@@ -493,12 +614,36 @@ const slider = function () {
 slider();
 
 //map click handler function
+const forcastActive = function (i) {
+  i += 1;
+  document.querySelector(`.panel3-day${i}`).style.flex = "5";
+  document.querySelector(`.panel3-icon-${i}`).style.transform =
+    "translateY(1.3rem) ";
+  document.querySelector(`.panel3-icon-${i}`).style.transform =
+    "translateY(1.3)";
+  document.querySelector(`#day${i}`).style.opacity = "1";
+
+  document.querySelector(`.panel3-weather-${i}`).style.opacity = "1";
+};
+const forcastNotActive = function (i) {
+  i += 1;
+  document.querySelector(`.panel3-day${i}`).style.flex = "2";
+  document.querySelector(`.panel3-icon-${i}`).style.transform = "translateY(0)";
+  document.querySelector(`#day${i}`).style.opacity = "0";
+
+  document.querySelector(`.panel3-icon-${i}`).style.transform = "translateY(0)";
+  document.querySelector(`.panel3-weather-${i}`).style.opacity = "0";
+};
 const panelMapHandler = function (el) {
+  for (let k = 0; k < 3; k++) {
+    forcastNotActive(k);
+  }
   panelUI(el);
   loader3.style.visibility = "visible";
-
+  loader4.style.visibility = "visible";
   setTimeout((e) => {
     loader3.style.visibility = "hidden";
+    loader4.style.visibility = "hidden";
   }, 2000);
 };
 //map click handler
@@ -531,16 +676,6 @@ map.on("click", function (e) {
     });
 });
 
-// fetch(
-//   `https://api.openweathermap.org/data/2.5/forecast?q=delhi&APPID=bf99cb0065e369f731cfedfe08b80061`
-// )
-//   .then((el) => {
-//     return el.json();
-//   })
-//   .then((el) => {
-//     console.log(el);
-//   });
-
 //narration
 
 let narCount = 1;
@@ -562,4 +697,28 @@ narrationContainer.addEventListener("click", (el) => {
   });
 
   narCount++;
+});
+
+//test
+// fetch(
+//   `http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?date=1527811200&opacity=0.9&fill_bound=true&appid=bf99cb0065e369f731cfedfe08b80061`
+// )
+//   .then((el) => {
+//     return el.json();
+//   })
+//   .then((el) => {
+//     console.log(el);
+//   });
+
+//Forcast panel
+
+const panelAll3 = document.querySelectorAll(".panel-day");
+
+panelAll3.forEach((el, i) => {
+  el.addEventListener("click", (e) => {
+    for (let k = 0; k < 3; k++) {
+      forcastNotActive(k);
+    }
+    forcastActive(i);
+  });
 });
